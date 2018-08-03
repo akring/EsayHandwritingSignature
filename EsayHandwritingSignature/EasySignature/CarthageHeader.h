c7b41713731a4e1c82e2397f6853a41408731988
吕俊 <lvjun@rlair.net>
2018-08-03 10:08:29 +0800

    Support Carthage

---
+++ EsayHandwritingSignature/EasySignature/CarthageHeader.h
@@ -0,0 +1,15 @@
+//
+//  CarthageHeader.h
+//  EsayHandwritingSignature
+//
+//  Created by 吕俊 on 2018/8/3.
+//  Copyright © 2018年 liang. All rights reserved.
+//
+
+#ifndef CarthageHeader_h
+#define CarthageHeader_h
+
+#import "PopSignatureView.h"
+#import "EasySignatureView.h"
+
+#endif /* CarthageHeader_h */
